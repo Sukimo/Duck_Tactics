e9dc17e61e291c15995d8f5948cdae30546e6447
Sukimo <areeyaodsup@gmail.com>
2026-04-19 16:39:30 +0700

    [update] melee , range
    melee_duck change attack-range to 100 to active melee
    range_attack can edit speed and arc_height

--- components/range_attack.gd
+++ components/range_attack.gd
@@ -3,6 +3,7 @@ class_name RangeAttackComponent
 
 const PROJECTILE_SCENE := "res://projectile/Projectile.tscn"
 @export var projectile_speed: float = 300.0
+@export var arc_height: float = 80.0
 
 var _projectile_scene: PackedScene = null
 
@@ -16,6 +17,7 @@ func do_attack(target: Node2D) -> void:
 		return
 	var start := _owner_node.global_position
 	var t_pos := target.global_position
+	#prediction shoot pos
 	var travel := start.distance_to(t_pos) / projectile_speed
 	var vel := Vector2.ZERO
 	if target.get("velocity") != null:
@@ -24,7 +26,13 @@ func do_attack(target: Node2D) -> void:
 
 	var proj = _projectile_scene.instantiate()
 	get_tree().current_scene.add_child(proj)
+	
 	if proj.get("damage") != null:
 		proj.damage = attack_damage
+	if proj.get("speed") !=null:
+		proj.speed = projectile_speed
+	if proj.get("arc_height") !=null:
+		proj.arc_height = arc_height
+		
 	if proj.has_method("init"):
 		proj.call("init", target, start, predicted)
